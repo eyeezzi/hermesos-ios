0de4b99127a05d6c6f0fee447a9d5aef9450f7ab
Uzziah Eyee <e.uzziah@gmail.com>
2018-08-07 04:27:08 -0230

    Added a constants scaffold to hold API endpoint values.

---
+++ hermesos/helpers/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  hermesos
+//
+//  Created by Uzziah on 2018-08-07.
+//  Copyright © 2018 eyeezzi. All rights reserved.
+//
+
+struct Endpoints {
+    static let BASE = ""
+    static let REQUEST_SMS = "" // get
+    static let SIGN_IN = "" // post
+    static let CREATE_ACCOUNT = "" // post
+    static let SOS = "" // get, post, delete
+    static let PROFILE = "" // get, post, delete
+}
